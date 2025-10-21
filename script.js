7a5de7577947a484b8bc9f377f0edb1c229da8d9
AdrGIT273 <adriankotkowski6@gmail.com>
2025-10-21 19:38:20 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+
+
+
+let i =0 ;
+
+let List = ["Jak wita się mleko",
+    "Jak nazywa się lina wyprodukowana w usa",
+    "jaka dziewczyna najczęściej kopie swoje ciuchy",
+    "Jaki jest najlepszy nauczyciel"
+];
+let odpowiedzi = ["Mlekovita","trampolina","kopciuszek","nieobecny"];
+
+let num; // globe
+function losuj() {
+    if(List.length === 0) {
+        document.getElementById("quest").innerText = "Brak więcej pytań! TWÓJ WYNIK TO " + i;
+        return;
+    }
+
+    num = Math.floor(Math.random() * List.length);
+    document.getElementById("quest").innerText = List[num];
+}
+
+function check() {
+    const odp = document.getElementById("answer").value;
+
+    if (odp.toLowerCase() === odpowiedzi[num].toLowerCase()) {
+        i++;
+        document.getElementById("quest").innerText = "Poprawna odpowiedź!";
+    } else {
+        document.getElementById("quest").innerText = "Zła odpowiedź!";
+        
+    }
+
+    // Wyczyść pole input
+    document.getElementById("answer").value = "";
+
+    // Usuń pytanie i odpowiedź z listy
+    List.splice(num, 1);
+    odpowiedzi.splice(num, 1);
+}
